46231133f3fb8d0b2c3d20df0fc96cf10d681a03
yasminehilout <yasmine.hilout@gmail.com>
2024-04-14 16:57:35 -0400

    Added link from profile back to home

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,19 +1,5 @@
 import React, { useState } from "react";
-import {
-  Card,
-  CardContent,
-  CardMedia,
-  Typography,
-  Box,
-  Avatar,
-  Tab,
-  Tabs,
-  List,
-  ListItem,
-  ListItemAvatar,
-  ListItemText,
-} from "@mui/material";
-import { Link } from "react-router-dom";
+import { Card, CardContent, CardMedia, Typography, Box, Avatar, Tab, Tabs, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
 
 const ProfileCard = ({ fName, lName, favorites, tried, image }) => {
   const [selectedTab, setSelectedTab] = useState(0);
@@ -25,25 +11,25 @@ const ProfileCard = ({ fName, lName, favorites, tried, image }) => {
   const recipesToShow = selectedTab === 0 ? favorites : tried;
 
   return (
-    <Card raised className="w-full max-w-lg bg-amber-50">
+    <Card raised className="w-full max-w-lg bg-coffee-dark">
       <CardContent className="p-0">
-      <Box className="p-4 text-center">
-          <Avatar src={image} alt={`${fName} ${lName}`} className="mx-auto" sx={{ width: 90, height: 90 }} />
-          <Typography variant="h5" className="mt-2">{fName} {lName}</Typography>
+      <Box className="p-4 text-center bg-coffee-dark">
+          <Avatar src={image} alt={`${fName} ${lName}`} className="mx-auto border-4 border-[#A79277]" sx={{ width: 90, height: 90 }} />
+          <Typography variant="h5" className="mt-2 font-semibold text-[#A79277]">{fName} {lName}</Typography>
         </Box>
 
         {/* Tabs */}
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs value={selectedTab} onChange={handleChange} centered>
-            <Tab label="Favorite Recipes" />
-            <Tab label="Tried Recipes" />
-          </Tabs>
+        <Box className="border-b border-[#D1BB9E]">
+            <Tabs value={selectedTab} onChange={handleChange} centered className="text-[#A79277]">
+                <Tab label="Favorite Recipes" className="hover:bg-[#EAD8C0] focus:bg-[#EAD8C0]" />
+                <Tab label="Tried Recipes" className="hover:bg-[#EAD8C0] focus:bg-[#EAD8C0]" />
+            </Tabs>
         </Box>
 
         {/* List of Recipes */}
-        <List sx={{ width: '100%' }}>
+        <List sx={{ width: '100%', backgroundColor: '#FFF2E1' }}>
           {recipesToShow.map((recipe, index) => (
-            <ListItem key={index}>
+            <ListItem key={index} className="border-b border-[#D1BB9E]">
               <ListItemAvatar>
                 <CardMedia
                   component="img"
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import ProfileCard from "../components/ProfileCard";
 import queryForFirstName from "../components/queryForFirstName";
 import { TextField, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 
 const Profile = () => {
@@ -21,7 +22,9 @@ const Profile = () => {
 
     return (
         <div className="grid grid-cols-2 gap-x-36 gap-y-24 mt-10">
-            <h1>Profiles</h1>
+            <Link to="/">
+                <Typography variant="h5" className="text-coffee-cream text-center mt-10">Back to Home</Typography>
+            </Link>
             {queriedProfile.map((profile, index) => <div key={index}><ProfileCard fName={profile.firstName} lName={profile.lastName} favorites={profile.favorites} tried={profile.tried} image={"/profilePic.jpg"} /></div>)}
         </div>
     );
