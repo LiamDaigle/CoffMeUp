83b135f9b42a292becb2730f1380db0ed582a2fd
yasminehilout <yasmine.hilout@gmail.com>
2024-04-14 16:13:31 -0400

    mock profile data

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import './App.css'
 import Profile from './pages/Profile';
 import SearchResultsPage from './pages/SearchResultsPage';
 import constructRecipe from './components/constructRecipe';
+import constructProfile from './components/constructProfile';
 import addRecipe from './components/addRecipe'
+import addProfile from './components/addProfile'
 
 function App() {
-  // Some default values you can use:
+  // Some default Recipes values you can use:
   // const obj1 = constructRecipe("Coffee", "This is a cup of coffee", [], [], "5.0")
   // const obj2 = constructRecipe("Iced Coffee", "This is a cup of iced coffee", [], [], "4.7")
   // const obj3 = constructRecipe("Mocha", "This is a cup of Mocha", [], [], "4.2")
@@ -17,6 +19,13 @@ function App() {
   // addRecipe(obj1)
   // addRecipe(obj2)
   // addRecipe(obj3)
+
+
+  // Some default Profile values you can use:
+  // const obj4 = constructProfile("Johna", "Dee", [obj1, obj2, obj3], [obj2, obj3])
+  // const obj5 = constructProfile("Jane", "Doe", [obj1, obj2], [obj1])
+  // addProfile(obj4)
+  // addProfile(obj5)
   
   return (
     <div className="h-screen w-screen">
