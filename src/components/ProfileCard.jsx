494b598206f47f108bfb7a93dfbe9a72aec632d3
yasminehilout <yasmine.hilout@gmail.com>
2024-04-14 22:05:51 -0400

    Place title next to profile pic

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -12,35 +12,35 @@ const ProfileCard = ({ fName, lName, favorites, tried, image }) => {
 
   return (
     <Card raised className="w-full bg-coffee-dark rounded-lg">
-      <CardContent className="p-0">
-      <Box className="p-4 text-center bg-coffee-dark">
-          <Avatar src={image} alt={`${fName} ${lName}`} className="mx-auto border-4 border-coffee-medium rounded-full" sx={{ width: 90, height: 90 }} />
-          <Typography variant="h5" className="mt-2 font-semibold text-coffee-medium">{fName} {lName}</Typography>
+        <CardContent className="p-0">
+        <Box className="flex items-center p-4 bg-coffee-dark">
+            <Avatar src={image} alt={`${fName} ${lName}`} className="border-4 border-coffee-medium rounded-full" sx={{ width: 90, height: 90 }} />
+            <Typography variant="h5" className="ml-4 font-semibold text-coffee-medium">{fName} {lName}</Typography>
         </Box>
 
         {/* Tabs */}
         <Box className="border-b border-coffee-medium">
             <Tabs value={selectedTab} onChange={handleChange} centered className="text-coffee-medium">
-                <Tab label="Favorite Recipes" className="hover:bg-coffee-light focus:bg-coffee-light" />
-                <Tab label="Tried Recipes" className="hover:bg-coffee-light focus:bg-coffee-light" />
+            <Tab label="Favorite Recipes" className="hover:bg-coffee-light focus:bg-coffee-light" />
+            <Tab label="Tried Recipes" className="hover:bg-coffee-light focus:bg-coffee-light" />
             </Tabs>
         </Box>
 
         {/* List of Recipes */}
         <List sx={{ width: '100%', backgroundColor: 'coffee-cream' }}>
-          {recipesToShow.map((recipe, index) => (
+            {recipesToShow.map((recipe, index) => (
             <ListItem key={index} className="border-b border-coffee-medium">
-              <ListItemAvatar>
+                <ListItemAvatar>
                 <CardMedia
-                  component="img"
-                  sx={{ width: 56, height: 56, borderRadius: '50%' }} // Assuming square images for recipes
-                  image="/Iced White Mocha.jpg" // Static image path for demonstration
-                  alt={recipe.title}
+                    component="img"
+                    sx={{ width: 56, height: 56, borderRadius: '50%' }} // Assuming square images for recipes
+                    image="/Iced White Mocha.jpg" // Static image path for demonstration
+                    alt={recipe.title}
                 />
-              </ListItemAvatar>
-              <ListItemText primary={recipe.title} className="text-coffee-dark"/>
+                </ListItemAvatar>
+                <ListItemText primary={recipe.title} className="text-coffee-dark"/>
             </ListItem>
-          ))}
+            ))}
         </List>
       </CardContent>
     </Card>
