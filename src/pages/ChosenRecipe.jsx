68fa1920ec3665df6228870ae96dd581bf7e653e
yasminehilout <yasmine.hilout@gmail.com>
2024-04-16 21:35:46 -0400

    todo

--- src/pages/ChosenRecipe.jsx
+++ src/pages/ChosenRecipe.jsx
@@ -22,13 +22,18 @@ const ChosenRecipe = () => {
         return <div>Loading...</div>;
     }
 
+    // TODO: add display if tried or not
+    // TODO: add button to set if tried/not
+    // TODO: add to recipe to tried of profile
+    // TODO: switch tried of recipe in recipes AND in recipe in selected
+
     return (
-        <div className="mt-10 w-full px-[2vw] py-[4vw] pb-[10vh] bg-coffee-lightgreen min-h-screen">
+        <div className="mt-10 w-full px-[2vw] py-[4vw] bg-coffee-lightgreen min-h-screen">
             <Link to="/recipes" className="flex items-center text-coffee-dark hover:text-coffee-green">
                 <FaArrowLeft size={20} className="inline mr-2"/>
                 <Typography variant="h6">Back to Recipes</Typography>
             </Link>
-            <div className="mt-10 px-[5vw] bg-coffee-cream">
+            <div className="mt-10 bg-coffee-cream">
                 <RecipeCard 
                     title={recipe.title} 
                     description={recipe.description} 
