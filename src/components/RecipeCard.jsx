adc25dcbe9c6f3992aac27d9c26edaa3476de335
yasminehilout <yasmine.hilout@gmail.com>
2024-04-16 21:17:37 -0400

    Add Recipe Card

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FaStar, FaRegStar } from 'react-icons/fa'; // FontAwesome icons
+import { FaStar, FaRegStar } from 'react-icons/fa';
 
 const RecipeCard = ({
     title,
@@ -13,51 +13,51 @@ const RecipeCard = ({
     const [isTried, setIsTried] = useState(tried);
     const numericRating = parseFloat(rating) || 0;
     const filledStars = Math.floor(numericRating);
-    const halfStars = (Math.round(numericRating) > filledStars) ? 1 : 0;
-    const emptyStars = 5 - filledStars - halfStars;
+    const emptyStars = 5 - filledStars;
 
     const toggleTried = () => setIsTried(!isTried);
 
     return (
         <div className="flex flex-col">
-            <div className="flex flex-row md:items-start">
+            <div className="flex flex-row items-start">
                 <div className="flex-none w-1/3">
                     <img src={image} alt={title} className="w-full h-auto rounded-lg"/>
                 </div>
-                <div className="flex-grow p-5">
-                    <div className="mb-4">
-                        <h1 className="text-4xl text-coffee-dark satisfy-regular">{title}</h1>
-                        <div className="flex text-dark"> {/* text-dark is for filled stars color */}
+                <div className="flex-grow p-5 flex flex-col justify-between">
+                    <div>
+                        <div className="flex justify-between items-start">
+                            <h1 className="text-4xl text-coffee-dark satisfy-regular">{title}</h1>
+                            <div className="text-right text-lg text-coffee-dark">
+                                <span className="font-bold">Prep Time:</span> 1 min |
+                                <span className="font-bold">  Completion Time:</span> 2 min |
+                                <span className="font-bold">  Serving Size:</span> 2 cups
+                            </div>
+                        </div>
+                        <div className="flex text-coffee-dark my-2">
                             {[...Array(filledStars)].map((_, i) => (
-                                <FaStar key={i} size={30} />
-                            ))}
-                            {[...Array(halfStars)].map((_, i) => (
                                 <FaStar key={i} size={30} />
                             ))}
                             {[...Array(emptyStars)].map((_, i) => (
                                 <FaRegStar key={i} size={30} />
                             ))}
                         </div>
-                        <div className="text-sm mt-2 text-coffee-dark">
-                            <span>Prep Time: 1 min</span> |  
-                            <span>Completion Time: 2 min</span> |  
-                            <span>Serving Size: 2 cups</span>
-                        </div>
+                    </div>
+                    <div>
                         <button 
                             onClick={toggleTried} 
-                            className={`mt-2 ${isTried ? 'bg-coffee-brown' : 'bg-coffee-light'} text-coffe-dark py-1 px-4 rounded`}
+                            className={`self-end mt-2 ${isTried ? 'bg-coffee-brown' : 'bg-coffee-light'} text-coffee-dark py-1 px-4 rounded`}
                         >
                             {isTried ? 'Tried' : 'Mark as Tried'}
                         </button>
+                        <h2 className="text-2xl text-coffee-dark satisfy-regular mt-4">Description</h2>
+                        <p className="text-coffee-dark sedan-regular">{description}</p>
                     </div>
-                    <h2 className="text-2xl text-coffee-dark satisfy-regular">Description</h2>
-                    <p className="text-coffee-dark sedan-regular">{description}</p>
                 </div>
             </div>
             <div className="flex flex-row">
                 <div className="w-1/2 p-5">
                     <h2 className="text-2xl text-coffee-dark satisfy-regular">Ingredients</h2>
-                    <ul className="list-disc text-coffee-dark sedan-regular px-[2vw]">
+                    <ul className="list-disc text-coffee-dark sedan-regular px-[1vw]">
                         {ingredients.map((ingredient, index) => (
                             <li 
                                 key={index} 
@@ -70,7 +70,7 @@ const RecipeCard = ({
                 </div>
                 <div className="w-1/2 p-5">
                     <h2 className="text-2xl text-coffee-dark satisfy-regular">Steps</h2>
-                    <ol className="list-decimal text-coffee-dark sedan-regular px-[2vw]">
+                    <ol className="list-decimal text-coffee-dark sedan-regular px-[1vw]">
                         {steps.map((step, index) => (
                             <li 
                                 key={index} 
